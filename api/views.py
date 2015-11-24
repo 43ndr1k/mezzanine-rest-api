41fb303c1aeb1b2dded10cac920489953a65b247
43ndr1k <dj-blackfire@gmx.de>
2015-11-24 13:47:42 +0100

    add api

---
+++ api/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+from rest_framework.decorators import list_route
+
+
+class BlogViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        return HttpResponse([{"info":"ficken"},])
+
+    @list_route()
+    def wordcloud(self, request):
+        return HttpResponse([{"text":"foo", "size":25}, {"text":"foobar", "size":17}, {"text":"bar", "size":12}, {"text":"shownotes", "size":5}])
